990937c1647b3195e14511e8985e117373974f8e
Rray <zrgift@gmail.com>
2017-07-27 12:13:47 +0800

    完善爬取内容

--- html_parser.py
+++ html_parser.py
@@ -19,13 +19,22 @@ class HtmlParser(object):
         repo_nodes = soup.find('ol', class_='repo-list').find_all('li')
         for repo_node in repo_nodes:
             repos_data = {}
-            title = repo_node.a.get_text().strip('\n')
+            title = repo_node.a.get_text(strip=True)
             title_arr = title.split('/')
             repos_data['owner'] = title_arr[0].strip()
             repos_data['name'] = title_arr[1].strip()
-            repos_data['summary'] = repo_node.p.get_text().strip('\n') if repo_node.p else ' '
+            repos_data['summary'] = repo_node.p.get_text(strip=True) if repo_node.p else ' '
             bottom_node = repo_node.select('div.text-gray.mt-2')
-            print(bottom_node[0].find('span', class_='repo-language-color').find_next_sibling("span").get_text() if bottom_node[0].find('span', class_='repo-language-color') else 'Unknown')
+            if bottom_node[0].find('span', class_='repo-language-color') is not None:
+                repos_data['language'] = bottom_node[0].find('span', class_='repo-language-color').find_next_sibling("span").get_text(strip=True)
+                color_str = bottom_node[0].find('span', class_='repo-language-color')['style']
+                repos_data['color'] = color_str[color_str.index('#'):-1]
+            else:
+                repos_data['language'] = 'Unknown'
+                repos_data['color'] = '#fff'
+            repos_data['stars'] = bottom_node[0].a.get_text(strip=True)
+            if bottom_node[0].find('span', class_='float-sm-right') is not None:
+                repos_data['stars_today'] = bottom_node[0].find('span', class_='float-sm-right').get_text(strip=True)
             repos.append(repos_data)
         res_data['data'] = repos
         return res_data
